f38b4e28b3a061c70a92f09e0d92e015d84813f6
gaailiunas <gailiunaslaurynas@tutanota.com>
2025-07-25 19:35:34 +0300

    initial commit

---
+++ src/disasm.c
@@ -0,0 +1,5 @@
+#include "disasm.h"
+
+void disasm(const unsigned char *instructions, size_t len)
+{
+}
---
+++ src/disasm.h
@@ -0,0 +1,8 @@
+#ifndef DISASM_H
+#define DISASM_H
+
+#include <stddef.h>
+
+void disasm(const unsigned char *instructions, size_t len);
+
+#endif // DISASM_H
